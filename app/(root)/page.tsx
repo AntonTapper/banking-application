3d18fe8bd3fe0e6609fc89e22af3599f03a056ee
AntonTapper <ant.tapper@gmail.com>
2024-07-15 20:59:55 +0200

    header-box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+
+  const loggedIn = { firstName: 'Anton' }
+
   return (
     <section className='home'>
         <div className='home-content'>
             <header className='home-header'>
-                Welcome, Anton
+              <HeaderBox 
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName || 'Guest'}
+                subtext="Access and manage your account and transactions efficiently"
+              />
             </header>
         </div>
     </section>
