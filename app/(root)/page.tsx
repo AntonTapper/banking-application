e982b1eda4b3f57b2376b05725136b9ea6fee830
AntonTapper <ant.tapper@gmail.com>
2024-07-23 22:57:28 +0200

    right sidebar and bank card wip

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
 
-  const loggedIn = { firstName: 'Anton' }
+  const loggedIn = { firstName: 'Anton', lastName: 'Tapper', email: 'ant.tapper@gmail.com' }
 
   return (
     <section className='home'>
@@ -23,7 +24,13 @@ const Home = () => {
                 totalCurrentBalance={1250.35}
               />
             </header>
+            RECENT TRANSACTIONS
         </div>
+        <RightSidebar 
+          user={loggedIn}
+          transactions={[]}
+          banks={[{}, {}]}
+        />
     </section>
   )
 }
