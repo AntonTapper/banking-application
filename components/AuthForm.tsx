ce6549b7912d770b0a8af21bfb54baee8f6485b6
AntonTapper <ant.tapper@gmail.com>
2024-08-12 16:51:09 +0200

    sign in wip

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,12 +20,14 @@ import {
 import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
+import { Loader2 } from 'lucide-react'
 
 
 
 const AuthForm = ({ type }: {type: string}) => {
 
     const [user, setUser] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
 
     
         // 1. Define your form.
@@ -41,7 +43,9 @@ const AuthForm = ({ type }: {type: string}) => {
         function onSubmit(values: z.infer<typeof authFormSchema>) {
           // Do something with the form values.
           // ✅ This will be type-safe and validated.
+          setIsLoading(true)
           console.log(values)
+          setIsLoading(false)
         }
 
   return (
@@ -70,63 +74,35 @@ const AuthForm = ({ type }: {type: string}) => {
 
               </div>
           ) : (
-              <>
-                  <Form {...form}>
-                      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                          <FormField
-                              control={form.control}
-                              name="email"
-                              render={({ field }) => (
-                                  <div className='form-item'>
-                                    <FormLabel className='form-label'>
-                                        Email
-                                    </FormLabel>
-                                    <div className='flex w-full'>
-                                        <FormControl>
-                                            <Input
-                                                placeholder='Enter your email'
-                                                className='input-class'
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage className='form-message mt-2'/>
-                                    </div>
-                                  </div>
-                              )}
-                          />
-                            <CustomInput
-                                control={form.control} name="username" label="username" placeholder="Enter your username"
-                            />
-                            <CustomInput
-                                control={form.control} name="password" label="password" placeholder="Enter your password"
-                            />
+                  <>
+                      <Form {...form}>
+                          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
 
-                          <FormField
-                              control={form.control}
-                              name="password"
-                              render={({ field }) => (
-                                  <div className='form-item'>
-                                    <FormLabel className='form-label'>
-                                        Password
-                                    </FormLabel>
-                                    <div className='flex w-full'>
-                                        <FormControl>
-                                            <Input
-                                                placeholder='Enter your password'
-                                                className='input-class'
-                                                type='password'
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage className='form-message mt-2'/>
-                                    </div>
-                                  </div>
-                              )}
-                          />
-                          <Button type="submit">Submit</Button>
-                      </form>
-                  </Form>
-              </>
+                              <CustomInput
+                                  control={form.control} name="email" label="Email" placeholder="Enter your email"
+                              />
+                              <CustomInput
+                                  control={form.control} name="password" label="Password" placeholder="Enter your password"
+                              />
+                              <div className='flex flex-col gap-4'>
+                                  <Button type="submit" disabled={isLoading} className='form-btn'>{isLoading ? (
+                                      <>
+                                          <Loader2 size={20} className='animate-spin' /> &nbsp;
+                                          Loading...
+                                      </>
+                                  ) : type === 'sign-in' ? 'Sign In' : 'Sign Up'}</Button>
+                              </div>
+
+                          </form>
+                      </Form>
+
+                      <footer className='flex justify-center gap-1'>
+                          <p className='text-14 font-normal text-gray-600'>{type === 'sign in' ? "Don't have an account?" : "Already have an account?"}</p>
+                          <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className='form-link'>
+                              {type === 'sign-in' ? 'Sign up' : 'Sign in'}
+                          </Link>
+                      </footer>
+                  </>
           )}
       </section>
   )
