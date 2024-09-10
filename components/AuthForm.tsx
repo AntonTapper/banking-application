d8805c9efb32af64ccadbc84ade5408ac635cca8
AntonTapper <ant.tapper@gmail.com>
2024-09-10 20:44:30 +0200

    wip

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -21,11 +21,13 @@ import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { signIn, signUp } from '@/lib/actions/user.actions'
 
 
 
 const AuthForm = ({ type }: {type: string}) => {
-
+    const router = useRouter()
     const [user, setUser] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
 
@@ -48,13 +50,16 @@ const AuthForm = ({ type }: {type: string}) => {
           setIsLoading(true)
           try {
             if(type === 'sign-up'){
-              const userData = {
-                firstName: data.firstName,
-              }
+              const newUser = await signUp(data)
+              setUser(newUser)
             }
 
             if(type === 'sign-in'){
-
+              const response = await signIn({
+                email: data.email,
+                password: data.password
+              })
+              if(response) router.push('/')
             }
           } catch (error) {
             console.log(error)
