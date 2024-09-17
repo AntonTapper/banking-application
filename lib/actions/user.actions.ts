05c08cf35c82aa8467b48a68d370e33f81e1279e
AntonTapper <ant.tapper@gmail.com>
2024-09-17 21:05:57 +0200

    wip

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,8 @@
 'use server'
 
+import { ID } from "node-appwrite"
+import { createAdminClient, createSessionClient } from "../appwrite"
+
 export const signIn = async () => {
     try {
         
@@ -9,9 +12,32 @@ export const signIn = async () => {
 }
 
 export const signUp = async (userData: SignUpParams) => {
+    const { email, password, firstName, lastName} = userData
     try {
-        
+        const { account } = await createAdminClient();
+
+        const newUserAccount = await account.create(ID.unique(), email, password, `${firstName} ${lastName}`);
+        const session = await account.createEmailPasswordSession(email, password);
+
+        cookies().set("my-custom-session", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        });
     } catch (error) {
         console.error('Error', error)
     }
 }
+
+// ... your initilization functions
+
+export async function getLoggedInUser() {
+    try {
+      const { account } = await createSessionClient();
+      return await account.get();
+    } catch (error) {
+      return null;
+    }
+  }
+  
