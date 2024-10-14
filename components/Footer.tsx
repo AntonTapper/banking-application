4ba87de49d8beaba820facaa6ed43422fc18b945
AntonTapper <ant.tapper@gmail.com>
2024-10-14 22:24:57 +0200

    footer wip

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Footer = () => {
+const Footer = ({ user }: FooterProps) => {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+        <div className='footer_name'>
+            <p>
+                 {user.firstName[0]}
+            </p>
+        </div>
+    </footer>
   )
 }
 
