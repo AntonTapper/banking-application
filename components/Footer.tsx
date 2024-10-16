d047d01ae37a373834581f67d988b207879d5baa
AntonTapper <ant.tapper@gmail.com>
2024-10-17 00:44:23 +0200

    wip

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
-const Footer = ({ user }: FooterProps) => {
+const Footer = ({ user, type = 'desktop' }: FooterProps) => {
   return (
     <footer className='footer'>
-        <div className='footer_name'>
-            <p>
-                 {"test"}
+        <div className={type === 'mobile' ? 'footer_name-mobile' : 'footer_name'}>
+            <p className='text-xl font-bold text-gray-700'>
+                 {"user.name"}
             </p>
+        </div>
+        
+        <div className={type === 'mobile' ? 'email-mobile' : 'footer_email'}>
+          <h1 className='text-14 truncate font-normal text-gray-600'>
+            {"user.name"}
+          </h1>
         </div>
     </footer>
   )
